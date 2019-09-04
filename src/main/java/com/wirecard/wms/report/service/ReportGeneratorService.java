75e6cbb7f7fc3b963074074815acd77f20b9ac57
denis prasetio <denisu08@gmail.com>
2019-09-04 14:26:13 +0700

    Add download report async method

--- src/main/java/com/wirecard/wms/report/service/ReportGeneratorService.java
+++ src/main/java/com/wirecard/wms/report/service/ReportGeneratorService.java
@@ -2,6 +2,15 @@ package com.wirecard.wms.report.service;
 
 
 import com.wirecard.wms.report.vo.User;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JsonDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,6 +18,14 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -28,6 +45,68 @@ public class ReportGeneratorService {
         User results = restTemplate.getForObject(url, User.class);
         // Artificial delay of 1s for demonstration purposes
         Thread.sleep(1000L);
+        return CompletableFuture.completedFuture(results);
+    }
+
+    @Async("reportExecutor")
+    public CompletableFuture<Map> downloadReport(Map<String, Object> parameterReport, JSONObject jsonData, JSONObject parameterJSON, boolean isImage, byte[] reportBytes) throws Exception {
+        JasperDesign design = null;
+        JasperPrint jasperPrint = null;
+        boolean isJasperMain = false;
+        String jasperReportBase64 = null;
+        JasperReport mainJasperReport = null;
+        if (isImage) {
+            design = JRXmlLoader.load(new ByteArrayInputStream(reportBytes));
+        } else {
+            List<Map> reportFiles = (List<Map>) parameterJSON.get("reportFiles");
+            for (Map item : reportFiles) {
+                if (item.get("jasper") != null) {
+                    byte[] jasperTmpBytes = Base64.getDecoder().decode((String) item.get("jasper"));
+                    if ((Boolean) item.getOrDefault("isMain", false)) {
+                        isJasperMain = true;
+                        jasperReportBase64 = new String(jasperTmpBytes);
+                        mainJasperReport = (JasperReport) JRLoader.loadObject(new ByteArrayInputStream(jasperTmpBytes));
+                    } else {
+                        parameterReport.put((String) item.get("keySubReport"), (JasperReport) JRLoader.loadObject(new ByteArrayInputStream(jasperTmpBytes)));
+                    }
+                } else {
+                    byte[] reportTmpBytes = Base64.getDecoder().decode((String) item.get("reportData"));
+                    if ((Boolean) item.getOrDefault("isMain", false)) {
+                        design = JRXmlLoader.load(new ByteArrayInputStream(reportTmpBytes));
+                    } else {
+                        parameterReport.put((String) item.get("keySubReport"), JasperCompileManager.compileReport(JRXmlLoader.load(new ByteArrayInputStream(reportTmpBytes))));
+                    }
+                }
+            }
+        }
+
+        JsonDataSource jsonDataSource = new JsonDataSource(new ByteArrayInputStream(jsonData.toString().getBytes()));
+        if (isJasperMain) {
+            jasperPrint = JasperFillManager.fillReport(mainJasperReport, parameterReport, jsonDataSource);
+        } else {
+            ByteArrayOutputStream bais = new ByteArrayOutputStream();
+            JasperCompileManager.compileReportToStream(design, bais);
+            jasperReportBase64 = new String(Base64.getEncoder().encode(bais.toByteArray()));
+            jasperPrint = JasperFillManager.fillReport(new ByteArrayInputStream(bais.toByteArray()), parameterReport, jsonDataSource);
+        }
+
+        JasperPrintManager printManager = JasperPrintManager.getInstance(DefaultJasperReportsContext.getInstance());
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        if (isImage) {
+            BufferedImage rendered_image = (BufferedImage) printManager.printPageToImage(jasperPrint, 0, 1.6f);
+            ImageIO.write(rendered_image, "png", outputStream);
+        } else {
+            JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+            exporter.exportReport();
+        }
+
+        Map results = new HashMap();
+        results.put("report", outputStream.toByteArray());
+        results.put("jasper", jasperReportBase64);
+
         return CompletableFuture.completedFuture(results);
     }
 }
