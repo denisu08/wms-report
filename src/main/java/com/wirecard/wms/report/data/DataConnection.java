d3072b392489f1a656743bdb318c719384e3eaec
denis prasetio <denisu08@gmail.com>
2019-09-21 16:39:46 +0700

    Fix generate report

--- src/main/java/com/wirecard/wms/report/data/DataConnection.java
+++ src/main/java/com/wirecard/wms/report/data/DataConnection.java
@@ -1,13 +1,19 @@
 package com.wirecard.wms.report.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.net.URL;
 import java.net.URLConnection;
 import javax.xml.bind.DatatypeConverter;
 
-public class DataConnection extends URLConnection {
+public class DataConnection extends URLConnection implements Serializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataConnection.class);
 
     public DataConnection(URL u) {
         super(u);
@@ -22,7 +28,7 @@ public class DataConnection extends URLConnection {
     public InputStream getInputStream() throws IOException {
         String data = url.toString();
         data = data.replaceFirst("^.*;base64,", "");
-        System.out.println("Data: " + data);
+        logger.info("DataConnection is parsed");
         byte[] bytes = DatatypeConverter.parseBase64Binary(data);
         return new ByteArrayInputStream(bytes);
     }
--- src/main/java/com/wirecard/wms/report/data/Handler.java
+++ src/main/java/com/wirecard/wms/report/data/Handler.java
@@ -1,11 +1,17 @@
 package com.wirecard.wms.report.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.io.Serializable;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
 
-public class Handler extends URLStreamHandler {
+public class Handler extends URLStreamHandler implements Serializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(Handler.class);
 
     @Override
     protected URLConnection openConnection(URL u) throws IOException {
@@ -13,11 +19,13 @@ public class Handler extends URLStreamHandler {
     }
 
     public static void install() {
+        logger.info("install");
         String pkgName = Handler.class.getPackage().getName();
         String pkg = pkgName.substring(0, pkgName.lastIndexOf('.'));
 
         String protocolHandlers = System.getProperty("java.protocol.handler.pkgs", "");
         if (!protocolHandlers.contains(pkg)) {
+            logger.info("!protocolHandlers.contains: " + pkg);
             if (!protocolHandlers.isEmpty()) {
                 protocolHandlers += "|";
             }
